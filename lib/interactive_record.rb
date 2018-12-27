6864cee7279bccdfaa76be8b2745163a85188576
Cheng Vue <cvu3@ucsc.edu>
2018-12-27 06:56:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,10 +52,10 @@ class InteractiveRecord
     sql = <<-SQL
       SELECT *
       FROM #{table_name}
-      WHERE name = #{name}
+      WHERE name = ?
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, "#{name}")
   end
 
 end
