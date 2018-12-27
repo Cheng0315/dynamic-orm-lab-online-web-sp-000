45e621bc818c71d98b2e269c4b2ceba8852883bc
Cheng Vue <cvu3@ucsc.edu>
2018-12-27 05:45:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,18 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  
+  def self.table_name
+    self.to_s.downcase.plurals
+  end
 
-
+  def self.column_name
+    DB[:conn].results_as_hash() = true
+    column_data = []
+    
+    sql = "PRAGMA table_info('#{table_name}')"
+    DB[:conn].execute(slq).each {|o| column_data << o[:name]}
+    
+    column.compact
+  end
 end
