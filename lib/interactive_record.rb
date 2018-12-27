94243099d9d8ddffe0e6490bf1e851fd09599676
Cheng Vue <cvu3@ucsc.edu>
2018-12-27 05:46:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,7 +8,7 @@ class InteractiveRecord
   end
 
   def self.column_name
-    DB[:conn].results_as_hash() = true
+    DB[:conn].results_as_hash = true
     column_data = []
 
     sql = "PRAGMA table_info('#{table_name}')"
